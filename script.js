14b5377142bc147ff3326d7ef14f07191d74b5bc
Rashel124 <webcoderitrashel124@gmail.com>
2025-02-11 04:16:02 -0800

    Js conditional Statement

---
+++ script.js
@@ -0,0 +1,42 @@
+
+
+let number = -746576;
+
+if (number > 0){
+    console.log ("This number is positive");
+}
+
+// else if (number < 0){
+//     console.log ("The number is negative");
+// }
+
+else{
+    console.log("The number is negative");
+}
+
+
+let number2 = 789;
+
+
+if (number2%2 == 0){
+    console.log ("This is an even number");
+}
+
+else{
+    console.log("This is an odd number");
+}
+
+
+let number1 =89;
+let number3 =67;
+
+if(number1 > number3){
+    console.log("number1 is maximum");
+
+}
+
+else{
+    console.log("nuber3 is maximum");
+}
+
+
